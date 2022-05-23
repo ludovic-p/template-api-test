02ed4155e46a66120b3dc29a4905b5bd0d752a98
Ludovic CP <cron.peverelly@gmail.com>
2022-05-23 23:05:09 +0200

    feat(sequelize): allow to disable the logs on sequelize part

--- src/middleware/sequelize.js
+++ src/middleware/sequelize.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite3'
+  storage: './database.sqlite3',
+  logging: process.env.SEQUELIZE_LOGS === 'true' ? console.log : false 
 });
 
 module.exports = sequelize;
