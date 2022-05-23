a2449d731b3a3b1b7b12fe12c3b952e58f36731d
Ludovic CP <cron.peverelly@gmail.com>
2022-05-23 23:17:51 +0200

    fix(getProfile): condition to check if the header profile_id exists added

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,10 +1,14 @@
 
 const getProfile = async(req, res, next) => {
-    const {Profile} = req.app.get('models');
-    const profile = await Profile.findOne({where: {id: req.get('profile_id') || 0}});
-    if(!profile) return res.status(401).end();
-    req.profile = profile;
-    next();
+    if (req.get('profile_id')) {
+        const { Profile } = req.app.get('models');
+        const profile = await Profile.findOne({ where: { id: req.get('profile_id') || 0 } });
+        if(!profile) return res.status(401).end()
+        req.profile = profile;
+        next();
+    } else {
+        return res.status(401).end()
+    }
 };
 
 module.exports = getProfile;
