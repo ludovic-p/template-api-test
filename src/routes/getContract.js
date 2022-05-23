074bde37f6240e4aa09cddd7c89161252516dfea
Ludovic CP <cron.peverelly@gmail.com>
2022-05-23 23:20:16 +0200

    fix(getContract): update filter on Sequelize request to get contract according to the profile_id header

--- src/routes/getContract.js
+++ src/routes/getContract.js
@@ -2,12 +2,12 @@ const getProfile = require('../middleware/getProfile');
 
 
 const getContract = (app) => {
-  app.get('/contracts/:id', getProfile, async (req, res) => {
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+  app.get('/contracts/:id', async (req, res) => {
+    const { Contract } = req.app.get('models');
+    const { id } = req.params;
+    const contract = await Contract.findOne({ where: { id, ClientId: req.profile.id } });
     if(!contract) return res.status(404).end()
-    res.json(contract)
+    res.json(contract);
   });
 };
 
