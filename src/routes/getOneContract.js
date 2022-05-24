af7abd38fe0df9113ab44494e40f2b6e0031327f
Ludovic CP <cron.peverelly@gmail.com>
2022-05-24 23:50:40 +0200

    fix(getOneContract): now we can get one Contract if the user is a contractor or a client

--- src/routes/getOneContract.js
+++ src/routes/getOneContract.js
@@ -1,9 +1,23 @@
+const { Op } = require('sequelize');
 
+/**
+ * Allow to get one contracts belongs to a profile
+ * @param {Object} app - Express application
+ * @returns {Contract} Array of Contracts object
+ */
 const getOneContract = (app) => {
   app.get('/contracts/:id', async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const contract = await Contract.findOne({ where: { id, ClientId: req.profile.id } });
+    const contract = await Contract.findOne({ 
+      where: { 
+        id, 
+        [Op.or]: [ 
+          { ClientId: req.profile.id }, 
+          { ContractorId: req.profile.id } 
+        ] 
+      } 
+    });
     if(!contract) return res.status(404).end()
     res.json(contract);
   });
